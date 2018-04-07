f69d378d5332d5a4d7d9397045a94647d174f935
Marco Silverio Castro <marcosilveriocastro@MacBook-Pro-de-Marco.local>
2018-04-07 01:04:44 -0500

    Data Inicial

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+
+-- Ambitos o permisos del usuario en las ordenes de pedido
+insert into scopes(name,nameSpring,createdAt,createdBY) values ('Crear ordenes de pedido','ROLE_CREATE_ORDERS',now(),'SYSTEM');
+insert into scopes(name,nameSpring,createdAt,createdBY) values ('Ver ordenes de pedido','ROLE_READ_ORDERS',now(),'SYSTEM');
+insert into scopes(name,nameSpring,createdAt,createdBY) values ('Modificar ordenes de pedido','ROLE_UPDATE_ORDERS',now(),'SYSTEM');
+insert into scopes(name,nameSpring,createdAt,createdBY) values ('Eliminar ordenes de pedido','ROLE_DELETE_ORDERS',now(),'SYSTEM');
+-- TODO Ambitos o permisos del usuario para gestionar su perfil
+-- USUARIO DEMO (El password es 123456)
+insert into users(fullName,userName,password,phone,createdAt,createdBY,enabled) values (
+    'Marco Silverio Castro','marcosilv7486@gmail.com','$2a$11$9Ylt0Uyz3T6ghHCiWP4mlOD2Q0AhuBxx156kRt8k8M4pRabbl2/X2',
+    '942696030',now(),'SYSTEM',true);
+-- Relacios scopes(permisos o ambitos) entre users(usuarios)
+insert into user_scopes(user_id,scope_id,createdAt,createdBY) VALUE (1,1,now(),'SYSTEM');
+insert into user_scopes(user_id,scope_id,createdAt,createdBY) VALUE (1,2,now(),'SYSTEM');
+insert into user_scopes(user_id,scope_id,createdAt,createdBY) VALUE (1,3,now(),'SYSTEM');
+insert into user_scopes(user_id,scope_id,createdAt,createdBY) VALUE (1,4,now(),'SYSTEM');
