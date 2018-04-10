4165fadb5b630ee9166d6e96571d048118d6af3b
Marco Silverio Castro <marcosilveriocastro@MacBook-Pro-de-Marco.local>
2018-04-10 16:26:11 -0500

    Cambio para generar .war

--- backend/src/main/java/com/marcosilv7/narutodelivery/NarutodeliveryApplication.java
+++ backend/src/main/java/com/marcosilv7/narutodelivery/NarutodeliveryApplication.java
@@ -2,13 +2,20 @@ package com.marcosilv7.narutodelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:custom.properties")
-public class NarutodeliveryApplication {
+public class NarutodeliveryApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NarutodeliveryApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(NarutodeliveryApplication.class, args);
 	}
 }
