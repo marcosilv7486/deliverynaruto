15ef5ab1008e321f59e18a06759a2b914d440766
robertito_9010@hotmail.com <Eduardo24>
2018-04-20 19:03:07 -0500

    Commit Historia Usuario - Direccion

--- android/app/src/main/java/com/marcosilv7/narutodelivery/SplashActivity.java
+++ android/app/src/main/java/com/marcosilv7/narutodelivery/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//
         setContentView(R.layout.activity_splash);
         TimerTask task = new TimerTask() {
             @Override
